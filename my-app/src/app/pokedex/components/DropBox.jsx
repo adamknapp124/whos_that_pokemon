35d9699cb73e88756592775659c1073380e60231
Adam Knapp <aknapp124@gmail.com>
2024-06-28 18:34:36 -0700

    Stat comparison added

--- my-app/src/app/pokedex/components/DropBox.jsx
+++ my-app/src/app/pokedex/components/DropBox.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from '../styles/dropbox.module.css';
 import ComparedImage from '@/app/components/ComparedImage';
+import { capitalize } from '@/app/utils/capitalize';
 
 const DropBox = ({ pokeName, handleDrop, handleDragOver, droppedPokemon }) => {
 	const officialArtwork =
 		droppedPokemon?.sprites.other['official-artwork'].front_default;
 	const hp = droppedPokemon?.stats[0].base_stat;
+	const attack = droppedPokemon?.stats[1].base_stat;
+	const defense = droppedPokemon?.stats[2].base_stat;
+	const specialAttack = droppedPokemon?.stats[3].base_stat;
+	const specialDefense = droppedPokemon?.stats[4].base_stat;
+	const speed = droppedPokemon?.stats[5].base_stat;
+	// hp, attack, defense, special-attack, special-defense, speed
 
 	return (
 		<>
@@ -17,7 +24,13 @@ const DropBox = ({ pokeName, handleDrop, handleDragOver, droppedPokemon }) => {
 				{droppedPokemon ? (
 					<>
 						<ComparedImage artwork={officialArtwork} />
-						<div>{hp}</div>
+						<div>{capitalize(pokeName)}</div>
+						<div>HP: {hp}</div>
+						<div>ATTACK: {attack}</div>
+						<div>DEFENSE: {defense}</div>
+						<div>SPECIAL ATTACK: {specialAttack}</div>
+						<div>SPECIAL DEFENSE: {specialDefense}</div>
+						<div>SPEED: {speed}</div>
 					</>
 				) : (
 					<div>Drop a pokemon here to view stats</div>
--- my-app/src/app/pokedex/components/PokemonGrid.jsx
+++ my-app/src/app/pokedex/components/PokemonGrid.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PokemonCard from './PokemonCard';
 import { getPokeData } from '@/app/api/Pokemons/route';
 
@@ -12,11 +12,43 @@ const PokemonGrid = ({ pokemon, captured, onDropPokemon }) => {
 	const [droppedPokeName2, setDroppedPokeName2] = useState('');
 	const [droppedPokemon1, setDroppedPokemon1] = useState(null);
 	const [droppedPokemon2, setDroppedPokemon2] = useState(null);
+	const [stats1, setStats1] = useState({
+		hp: 0,
+		attack: 0,
+		defense: 0,
+		specialAttack: 0,
+		specialDefense: 0,
+		speed: 0,
+	});
+	const [stats2, setStats2] = useState({
+		hp: 0,
+		attack: 0,
+		defense: 0,
+		specialAttack: 0,
+		specialDefense: 0,
+		speed: 0,
+	});
+	const [barLengths, setBarLengths] = useState({
+		hp: { p1: 0, p2: 0 },
+		attack: { p1: 0, p2: 0 },
+		defense: { p1: 0, p2: 0 },
+		specialAttack: { p1: 0, p2: 0 },
+		specialDefense: { p1: 0, p2: 0 },
+		speed: { p1: 0, p2: 0 },
+	});
 
 	const handleDrop1 = async (pokeName) => {
-		console.log(pokeName);
 		setDroppedPokeName1(pokeName);
 		const data = await getPokeData(pokeName);
+		const newStats = {
+			hp: data.stats[0].base_stat,
+			attack: data.stats[1].base_stat,
+			defense: data.stats[2].base_stat,
+			specialAttack: data.stats[3].base_stat,
+			specialDefense: data.stats[4].base_stat,
+			speed: data.stats[5].base_stat,
+		};
+		setStats1(newStats);
 		setDroppedPokemon1(data);
 
 		await fetch('/api/updateDropped', {
@@ -28,9 +60,17 @@ const PokemonGrid = ({ pokemon, captured, onDropPokemon }) => {
 		});
 	};
 	const handleDrop2 = async (pokeName) => {
-		console.log(pokeName);
 		setDroppedPokeName2(pokeName);
 		const data = await getPokeData(pokeName);
+		const newStats = {
+			hp: data.stats[0].base_stat,
+			attack: data.stats[1].base_stat,
+			defense: data.stats[2].base_stat,
+			specialAttack: data.stats[3].base_stat,
+			specialDefense: data.stats[4].base_stat,
+			speed: data.stats[5].base_stat,
+		};
+		setStats2(newStats);
 		setDroppedPokemon2(data);
 
 		await fetch('/api/updateDropped', {
@@ -72,33 +112,76 @@ const PokemonGrid = ({ pokemon, captured, onDropPokemon }) => {
 		e.dataTransfer.setData('text/plain', pokeName);
 	}
 
+	const calculateBarLength = (stat1, stat2) => {
+		const total = stat1 + stat2;
+		return {
+			p1: (stat1 / total) * 100,
+			p2: (stat2 / total) * 100,
+		};
+	};
+
+	useEffect(() => {
+		const newBarLengths = {
+			hp: calculateBarLength(stats1.hp, stats2.hp),
+			attack: calculateBarLength(stats1.attack, stats2.attack),
+			defense: calculateBarLength(stats1.defense, stats2.defense),
+			specialAttack: calculateBarLength(stats1.specialAttack, stats2.specialAttack),
+			specialDefense: calculateBarLength(
+				stats1.specialDefense,
+				stats2.specialDefense
+			),
+			speed: calculateBarLength(stats1.speed, stats2.speed),
+		};
+		setBarLengths(newBarLengths);
+	}, [stats1, stats2]);
+
 	const capturedPokemonNames = new Map(captured.map((mon) => [mon.name, mon.type]));
 
 	return (
 		<main className={styles.main}>
-			<DropBox
-				pokeName={droppedPokeName1}
-				handleDrop={handleDropEvent1}
-				handleDragOver={handleDragOver}
-				droppedPokemon={droppedPokemon1}
-			/>
-			<section className={styles.center}>
-				{pokemon.map((poke, index) => (
-					<PokemonCard
-						poke={poke}
-						key={index}
-						isCaptured={capturedPokemonNames.has(poke.name)}
-						type={capturedPokemonNames.get(poke.name)}
-						handleOnDrag={handleOnDrag}
-					/>
+			<div>
+				{Object.keys(barLengths).map((stat) => (
+					<div className={styles.compareBox} key={stat}>
+						<div
+							id={`${stat}-bar-1`}
+							style={{ width: `${barLengths[stat].p1}%` }}
+							className={styles.statBar1}>
+							{stats1[stat]}
+						</div>
+						<div
+							id={`${stat}-bar-2`}
+							style={{ width: `${barLengths[stat].p2}%` }}
+							className={styles.statBar2}>
+							{stats2[stat]}
+						</div>
+					</div>
 				))}
-			</section>
-			<DropBox
-				pokeName={droppedPokeName2}
-				handleDrop={handleDropEvent2}
-				handleDragOver={handleDragOver}
-				droppedPokemon={droppedPokemon2}
-			/>
+			</div>
+			<div className={styles.pokeBox}>
+				<DropBox
+					pokeName={droppedPokeName1}
+					handleDrop={handleDropEvent1}
+					handleDragOver={handleDragOver}
+					droppedPokemon={droppedPokemon1}
+				/>
+				<section className={styles.center}>
+					{pokemon.map((poke, index) => (
+						<PokemonCard
+							poke={poke}
+							key={index}
+							isCaptured={capturedPokemonNames.has(poke.name)}
+							type={capturedPokemonNames.get(poke.name)}
+							handleOnDrag={handleOnDrag}
+						/>
+					))}
+				</section>
+				<DropBox
+					pokeName={droppedPokeName2}
+					handleDrop={handleDropEvent2}
+					handleDragOver={handleDragOver}
+					droppedPokemon={droppedPokemon2}
+				/>
+			</div>
 		</main>
 	);
 };
